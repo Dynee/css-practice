3671a7796714a1b4c805d77a09daba4170a7289f
Dynee <lucaspadden@gmail.com>
2020-10-23 14:39:32 -0400

    still working on mobile menu

--- canoe-css/index.js
+++ canoe-css/index.js
@@ -1,14 +1,14 @@
-const menu = document.querySelector('#nav-menu');
-const burgerIcon = document.querySelector('#burger-icon');
+const menu = document.querySelector('.nav-menu');
+const navOpener = document.querySelector('.nav-opener');
 
 window.onload = function() {
     if (window.innerWidth <= 768) {
         menu.classList.add('hidden');
-        burgerIcon.classList.remove('hidden');
+        navOpener.classList.remove('hidden');
     }
 }
 
-burgerIcon.addEventListener('click', function() {
+navOpener.addEventListener('click', function() {
     if (menu.classList.contains('hidden')) {
         menu.classList.remove('hidden');
     } else {
@@ -19,9 +19,9 @@ burgerIcon.addEventListener('click', function() {
 window.addEventListener('resize', function() {
     if (window.innerWidth <= 768) {
         menu.classList.add('hidden');
-        burgerIcon.classList.remove('hidden');
+        navOpener.classList.remove('hidden');
     } else {
         menu.classList.remove('hidden');
-        burgerIcon.classList.add('hidden');
+        navOpener.classList.add('hidden');
     }
 })
