c620abb3ae3cb55986b1d05436af5938f9bd02d9
Dynee <lucaspadden@gmail.com>
2020-10-23 15:56:06 -0400

    got mobile menu working just need to finish styling it

--- canoe-css/index.js
+++ canoe-css/index.js
@@ -1,27 +1,24 @@
-const menu = document.querySelector('.nav-menu');
+const mobileMenu = document.querySelector('.nav-menu-mobile');
 const navOpener = document.querySelector('.nav-opener');
-
-window.onload = function() {
-    if (window.innerWidth <= 768) {
-        menu.classList.add('hidden');
-        navOpener.classList.remove('hidden');
-    }
-}
-
-navOpener.addEventListener('click', function() {
-    if (menu.classList.contains('hidden')) {
-        menu.classList.remove('hidden');
-    } else {
-        menu.classList.add('hidden');
-    }
-})
+const menu = document.querySelector('.nav-menu');
 
 window.addEventListener('resize', function() {
-    if (window.innerWidth <= 768) {
-        menu.classList.add('hidden');
-        navOpener.classList.remove('hidden');
-    } else {
-        menu.classList.remove('hidden');
-        navOpener.classList.add('hidden');
+  if (window.innerWidth <= 768) {
+    menu.classList.add('hidden');
+    navOpener.classList.remove('hidden');
+  } else {
+    menu.classList.remove('hidden');
+    navOpener.classList.add('hidden');
+    if (!(mobileMenu.classList.contains('hidden'))) {
+      mobileMenu.classList.add('hidden');
     }
+  }
+})
+
+navOpener.addEventListener('click', function() {
+  if (mobileMenu.classList.contains('hidden')) {
+    mobileMenu.classList.remove('hidden');
+  } else {
+    mobileMenu.classList.add('hidden');
+  }
 })
