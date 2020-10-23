e71ba0b20e4ca106bb6d0b4b31c11ff56ccf6194
Dynee <lucaspadden@gmail.com>
2020-10-23 18:36:01 -0400

    finished mobile menu

--- canoe-css/index.js
+++ canoe-css/index.js
@@ -2,10 +2,20 @@ const mobileMenu = document.querySelector('.nav-menu-mobile');
 const navOpener = document.querySelector('.nav-opener');
 const menu = document.querySelector('.nav-menu');
 
+window.onload = function() {
+  if (window.innerWidth <= 768) {
+    if (navOpener.classList.contains('hidden')) {
+      navOpener.classList.remove('hidden');
+    }
+    menu.classList.add('hidden');
+  }
+}
+
 window.addEventListener('resize', function() {
   if (window.innerWidth <= 768) {
     menu.classList.add('hidden');
     navOpener.classList.remove('hidden');
+    mobileMenu.classList.remove('hidden');
   } else {
     menu.classList.remove('hidden');
     navOpener.classList.add('hidden');
